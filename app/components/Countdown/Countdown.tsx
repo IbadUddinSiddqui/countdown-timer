01b11e4811669c841bed23e67d855e13bb56938f
Ibad Uddin <ibaduddinsiddiqui418@gmail.com>
2024-11-26 08:07:11 +0500

    timer

--- app/components/Countdown/Countdown.tsx
+++ app/components/Countdown/Countdown.tsx
@@ -59,26 +59,28 @@ function Countdown() {
           type="number"
           value={inputValue === 0 ? "" : inputValue} // Set to empty string if input is 0 (default)
           onChange={(e) => setInputValue(Number(e.target.value))}
-          className="w-56 text-center py-2 px-4 rounded-md border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-72 text-center py-2 px-4 rounded-md border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Enter Your Time seconds"
         />
-        <Button variant="ghost" onClick={setNewTime} className="bg-black text-white rounded-md py-2 px-6 transition-all duration-300 ease-in-out hover:bg-gray-800 hover:text-white">
-          Set Time
-        </Button>
+        
       </div>
   
       {/* Control Buttons */}
       <div className="flex gap-6 justify-center">
+      <Button variant="ghost" onClick={setNewTime} className="bg-black text-white rounded-md py-2 px-6 transition-all duration-300 ease-in-out hover:bg-gray-800 hover:text-white">
+            Set Time
+          </Button>
         {!isRunning ? (
-          <Button onClick={startTimer} className="bg-green-500 text-white rounded-md py-2 px-6 transition-all duration-300 ease-in-out hover:bg-green-600">
+          
+          <Button onClick={startTimer} className="bg-black text-white rounded-md py-2 px-6 transition-all duration-300 ease-in-out hover:bg-gray-800 hover:text-white">
             Start
           </Button>
         ) : (
-          <Button onClick={pauseTimer} className="bg-yellow-500 text-white rounded-md py-2 px-6 transition-all duration-300 ease-in-out hover:bg-yellow-600">
+          <Button onClick={pauseTimer} className="bg-black text-white rounded-md py-2 px-6 transition-all duration-300 ease-in-out hover:bg-gray-800 hover:text-white">
             Pause
           </Button>
         )}
-        <Button onClick={resetTimer} className="bg-red-500 text-white rounded-md py-2 px-6 transition-all duration-300 ease-in-out hover:bg-red-600">
+        <Button onClick={resetTimer} className="bg-black text-white rounded-md py-2 px-6 transition-all duration-300 ease-in-out hover:bg-gray-800 hover:text-white">
           Reset
         </Button>
       </div>
