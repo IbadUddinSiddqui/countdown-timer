4f10bd8d3ded3ab9d7f257ed238e2d06a6f71406
Ibad Uddin <ibaduddinsiddiqui418@gmail.com>
2024-11-27 04:15:29 +0500

    white

--- app/components/Countdown/Countdown.tsx
+++ app/components/Countdown/Countdown.tsx
@@ -118,14 +118,14 @@ function Countdown() {
             type="number"
             value={inputValue === 0 ? "" : inputValue}
             onChange={(e) => setInputValue(Number(e.target.value))}
-            className="w-full text-center py-2 px-4 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full text-center py-2 px-4 rounded-md border text-black dark:text-white border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Enter Time (seconds)"
           />
           <Input
             type="text"
             value={eventName}
             onChange={(e) => setEventName(e.target.value)}
-            className="w-full text-center py-2 px-4 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full text-center py-2 px-4 rounded-md border border-gray-300  text-black dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Event Name/Description"
           />
         </div>
